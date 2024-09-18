484d78f8e87f889f5cf5d0215b675f41c81be56f
surajverma2587 <surajverma2587@gmail.com>
2024-09-18 10:00:03 +0100

    styled components

---
+++ concepts/03-styled-components/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Banner from "./components/Banner.vue";
+</script>
+
+<template>
+  <Banner />
+</template>
+
+<style scoped>
+body {
+  margin: 0px;
+}
+</style>
---
+++ concepts/03-styled-components/src/components/Banner.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="banner">
+    <h1 class="title">Hello from Vue JS</h1>
+    <h2 class="sub-title">Let us begin...</h2>
+  </section>
+</template>
+
+<style scoped>
+.title {
+  color: #41b883;
+}
+
+.sub-title {
+  color: #34495e;
+}
+
+.banner {
+  padding: 32px;
+  text-align: center;
+}
+</style>
