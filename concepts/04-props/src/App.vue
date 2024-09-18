591ae0e322bdd85c4c412eaa3afb20a689436eb3
surajverma2587 <surajverma2587@gmail.com>
2024-09-18 10:24:02 +0100

    props

---
+++ concepts/04-props/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Banner from "./components/Banner.vue";
+</script>
+
+<template>
+  <Banner title="Hello from Vue JS" subTitle="Let us begin..." />
+  <Banner title="Hello from React JS" subTitle="Wait until next week..." />
+  <Banner title="Hello from Vanilla JS" subTitle="We are done..." />
+</template>
+
+<style scoped>
+body {
+  margin: 0px;
+}
+</style>
