89d2a511051b86e44a96b34b3dd02491ee4f285f
surajverma2587 <surajverma2587@gmail.com>
2024-09-18 11:15:40 +0100

    data binding

---
+++ concepts/05-data-binding/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Banner from "./components/Banner.vue";
+import AboutMe from "./components/AboutMe.vue";
+</script>
+
+<template>
+  <Banner title="My Portfolio" subTitle="All about me..." />
+  <AboutMe
+    firstName="Bob"
+    lastName="Smith"
+    profileImgUrl="https://images.unsplash.com/photo-1723117779554-86a2207307f3?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    email="bob.smith@email.com"
+    bio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos eaque deserunt nisi modi, velit deleniti harum optio repudiandae quis rem sapiente incidunt numquam dolores repellendus ipsam, non, voluptates natus. Minus."
+  />
+</template>
+
+<style scoped>
+body {
+  margin: 0px;
+}
+</style>
---
+++ concepts/05-data-binding/src/components/AboutMe.vue
@@ -0,0 +1,79 @@
+<template>
+  <section>
+    <h3 class="title">{{ firstName }} {{ lastName }}</h3>
+    <div class="container">
+      <div class="image-item">
+        <img :src="profileImgUrl" alt="profile picture for user" class="img" />
+      </div>
+      <div class="content-item">
+        <p>
+          {{ bio }}
+        </p>
+      </div>
+    </div>
+    <div class="links-container">
+      <a :href="`mailto:${email}`">Email Me</a>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.title {
+  text-align: center;
+}
+
+.container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.image-item {
+  flex: 1;
+  margin: 0px 16px;
+}
+
+.content-item {
+  flex: 2;
+  margin: 0px 16px;
+}
+
+.img {
+  object-fit: contain;
+  width: 100%;
+}
+
+p {
+  margin: 0;
+}
+
+.links-container {
+  text-align: center;
+  padding: 20px;
+}
+</style>
+
+<script setup>
+const props = defineProps({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  profileImgUrl: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
+</script>
