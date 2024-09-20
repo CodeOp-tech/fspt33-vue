e89988a4a1f977ab8118db7900fb78dc356b66c6
surajverma2587 <surajverma2587@gmail.com>
2024-09-20 09:02:57 +0100

    conditional render

---
+++ concepts/06-conditional-render/src/components/AboutMe.vue
@@ -0,0 +1,89 @@
+<template>
+  <section>
+    <h3 class="title">{{ firstName }} {{ lastName }}</h3>
+    <div class="container">
+      <div class="image-item">
+        <img :src="profileImgUrl" alt="profile picture for user" class="img" />
+      </div>
+      <div class="content-item">
+        <p>
+          {{ bio }}
+        </p>
+      </div>
+    </div>
+    <div class="links-container">
+      <a :href="`mailto:${email}`">Email Me</a>
+      <a v-if="socialMedia" :href="`mailto:${email}`">Social Media</a>
+      <a v-else>No Link</a>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.title {
+  text-align: center;
+}
+
+.container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.image-item {
+  flex: 1;
+  margin: 0px 16px;
+}
+
+.content-item {
+  flex: 2;
+  margin: 0px 16px;
+}
+
+.img {
+  object-fit: contain;
+  width: 100%;
+}
+
+p {
+  margin: 0;
+}
+
+a {
+  padding: 16px;
+}
+
+.links-container {
+  text-align: center;
+  padding: 20px;
+}
+</style>
+
+<script setup>
+const props = defineProps({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  profileImgUrl: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  socialMedia: {
+    type: String,
+    required: false,
+  },
+});
+</script>
