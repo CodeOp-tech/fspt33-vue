1823081658214139802ed304e8206a2bcb743d4f
surajverma2587 <surajverma2587@gmail.com>
2024-09-20 09:19:06 +0100

    render list

---
+++ concepts/07-render-list/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Countries from "./components/Countries.vue";
+
+const visitedCountries = ["Country 1", "Country 2", "Country 3"];
+
+const wishListCountries = ["Country A", "Country B"];
+</script>
+
+<template>
+  <h1>Places I have visited</h1>
+  <Countries :countries="visitedCountries" />
+
+  <h1>Places I have wish to visit</h1>
+  <Countries :countries="wishListCountries" />
+</template>
+
+<style scoped>
+body {
+  margin: 0px;
+}
+</style>
