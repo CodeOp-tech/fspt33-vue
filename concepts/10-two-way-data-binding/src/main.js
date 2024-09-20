1a808f48a296bc6bd7fbd8c91c8826499aa2050b
surajverma2587 <surajverma2587@gmail.com>
2024-09-20 10:15:25 +0100

    2 way data binding

---
+++ concepts/10-two-way-data-binding/src/main.js
@@ -0,0 +1,11 @@
+// import createApp
+import { createApp } from "vue";
+
+// import my App
+import App from "./App.vue";
+
+// create app
+const app = createApp(App);
+
+// mount the app
+app.mount("#app");
