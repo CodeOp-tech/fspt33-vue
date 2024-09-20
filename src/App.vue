c31d39e6d52cdf2bf1bafd09eb7d7109d662627d
surajverma2587 <surajverma2587@gmail.com>
2024-09-20 09:18:55 +0100

    render list

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,17 @@
 <script setup>
-import Banner from "./components/Banner.vue";
-import AboutMe from "./components/AboutMe.vue";
+import Countries from "./components/Countries.vue";
+
+const visitedCountries = ["Country 1", "Country 2", "Country 3"];
+
+const wishListCountries = ["Country A", "Country B"];
 </script>
 
 <template>
-  <Banner title="My Portfolio" subTitle="All about me..." />
-  <AboutMe
-    firstName="Bob"
-    lastName="Smith"
-    profileImgUrl="https://images.unsplash.com/photo-1723117779554-86a2207307f3?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    email="bob.smith@email.com"
-    bio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos eaque deserunt nisi modi, velit deleniti harum optio repudiandae quis rem sapiente incidunt numquam dolores repellendus ipsam, non, voluptates natus. Minus."
-  />
-  <AboutMe
-    firstName="Bob"
-    lastName="Smith"
-    profileImgUrl="https://images.unsplash.com/photo-1723117779554-86a2207307f3?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    email="bob.smith@email.com"
-    bio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos eaque deserunt nisi modi, velit deleniti harum optio repudiandae quis rem sapiente incidunt numquam dolores repellendus ipsam, non, voluptates natus. Minus."
-    socialMedia="https://www.google.co.uk/"
-  />
+  <h1>Places I have visited</h1>
+  <Countries :countries="visitedCountries" />
+
+  <h1>Places I have wish to visit</h1>
+  <Countries :countries="wishListCountries" />
 </template>
 
 <style scoped>
