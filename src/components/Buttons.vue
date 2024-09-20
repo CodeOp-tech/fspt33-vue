ab030e5edbef2f7820eb850a679f021c16a3c2d0
surajverma2587 <surajverma2587@gmail.com>
2024-09-20 10:05:03 +0100

    emit

---
+++ src/components/Buttons.vue
@@ -0,0 +1,9 @@
+<template>
+  <button @click="emit('handleClick', 'Red button clicked')">Red</button>
+  <button @click="emit('handleClick', 'Green button clicked')">Green</button>
+  <button @click="emit('handleClick', 'Blue button clicked')">Blue</button>
+</template>
+
+<script setup>
+const emit = defineEmits(["handleClick"]);
+</script>
