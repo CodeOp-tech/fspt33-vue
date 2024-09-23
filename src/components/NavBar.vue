32521a28ae35778fc5b82f6ca51251a464db1a92
surajverma2587 <surajverma2587@gmail.com>
2024-09-23 09:26:31 +0100

    styled navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,3 +1,28 @@
 <template>
-  <div>NavBar</div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <span class="navbar-brand">Bubbles</span>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <button class="btn btn-link nav-link active">Manager</button>
+          </li>
+          <li class="nav-item">
+            <button class="btn btn-link nav-link">Employee</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
